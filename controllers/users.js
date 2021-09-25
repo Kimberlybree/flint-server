0b720576d8070f6c8f36a6ae500a7211621404c8
jexxer <jexxe@live.com>
2021-09-24 23:48:59 -0400

    implemented jwt

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,28 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/userModel")
+
+// verify function for jwt authentication
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, 'mySecretKey', (err, user) => {
+            if(err){
+                return res.status(401).json({"message": "Token is not valid"})
+            }
+
+            req.user = user;
+            next()
+        })
+    } else {
+        res.status(401).json({"message": "Invalid Authentication"})
+    }
+}
 
 // Get ALL Users
 router.get("/", (req, res, next) => {
@@ -35,14 +55,19 @@ router.post('/', async (req, res, next) => {
 
 // Sign In Authentication
 router.post('/login', async (req, res, next) => {
-    User.findOne({username: req.body.username})
+    User.findOne({email: req.body.email})
         .then(async (user) => {
-            if(user == null){
+            if(user === null){
                 return res.status(400).send("User doesn't exist")
             }
             const match = await bcrypt.compare(req.body.password, user.password)
             if(match){
-                res.json(user)
+                // Generate an access token
+                const accessToken = jwt.sign({id: user._id, email: user.email}, 'mySecretKey')
+                res.json({
+                    userobj: user,
+                    accessToken,
+                })
             } else {
                 res.send('Not Allowed')
             }
@@ -59,9 +84,21 @@ router.put('/:id', (req, res, next) => {
 })
 
 // Delete a User and all related data
-router.delete('/:id', (req, res, next) => {
-    User.findOneAndDelete({_id: req.params.id})
-        .then((user) => res.json(user))
+router.delete('/:id', verify, (req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        res.status(200).json({"message": "User has been deleted"})
+    } else {
+        res.status(403).json({"message": "You are not allowed to delete this user"})
+    }
+    // User.findOneAndDelete({_id: req.params.id})
+    //     .then((user) => res.json(user))
+    //     .catch(next)
+})
+
+// Get individual Transaction of User
+router.get('/:id/gettransaction/:tid', (req, res, next) => {
+    User.findById({_id: req.params.id})
+        .then((user) => res.json(user.transactions))
         .catch(next)
 })
 
